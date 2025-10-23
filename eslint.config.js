11a086d3f8622b029bed354cfbe76408688b604c
Kenji Saito <1867845+poad@users.noreply.github.com>
2025-10-24 08:26:32 +0900

    Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,68 @@
+// @ts-check
+
+import { defineConfig } from 'eslint/config';
+import eslint from '@eslint/js';
+import stylistic from '@stylistic/eslint-plugin';
+import tseslint from 'typescript-eslint';
+import importPlugin from 'eslint-plugin-import';
+
+import pluginPromise from 'eslint-plugin-promise'
+
+import { includeIgnoreFile } from '@eslint/compat';
+import path from 'node:path';
+import { fileURLToPath } from 'node:url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+export default defineConfig(
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: [
+      '**/*.d.ts',
+      '**/*.css',
+      'node_modules/**/*',
+      'out',
+      'cdk.out',
+      'dist',
+    ],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.strict,
+  ...tseslint.configs.stylistic,
+  // @ts-expect-error ignore type error
+  pluginPromise.configs['flat/recommended'],
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: __dirname,
+      },
+    },
+    settings: {
+      'import/resolver': {
+        typescript: true,
+        node: true,
+      },
+    },
+    extends: [importPlugin.flatConfigs.recommended, importPlugin.flatConfigs.typescript],
+    plugins: {
+      '@stylistic': stylistic,
+      '@stylistic/js': stylistic,
+      '@stylistic/ts': stylistic,
+    },
+    rules: {
+      '@stylistic/semi': ['error', 'always'],
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/ts/indent': ['error', 2],
+      '@stylistic/comma-dangle': ['error', 'always-multiline'],
+      '@stylistic/arrow-parens': ['error', 'always'],
+      '@stylistic/quotes': ['error', 'single'],
+    },
+  },
+);
